08b7e06576f5c71cc19c4eb9050d249b2fe9a9fb
Rbravolibtech <122310916+Rbravolibtech@users.noreply.github.com>
2024-01-09 12:22:57 -0700

    assigned a value based on the presence or absence of a value in the nullable DateTime

--- Program.cs
+++ Program.cs
@@ -5,13 +5,30 @@ namespace NULLTypes
     internal class Program
     {
         static void Main(string[] args)
+
         {
 
-            DateTime? date = new DateTime(2024, 1, 9);
-            DateTime? date2 = date.GetValueOrDefault();
-            DateTime? date3 = date2;
+            DateTime? date = null;
+            DateTime date2;
 
-            Console.WriteLine(date3.GetValueOrDefault());
+            if (date != null)
+                date2 = date.GetValueOrDefault();
+            else
+                date2 = DateTime.Today;
+            Console.WriteLine(date2);
+
+
+
+            // this code initializes a nullable DateTime variable, assigns its value
+            //to another nullable DateTime variable using GetValueOrDefault()
+
+            //DateTime? date = new DateTime(2024, 1, 9);
+            //DateTime? date2 = date.GetValueOrDefault();
+            //DateTime? date3 = date2;
+
+            //Console.WriteLine(date3.GetValueOrDefault());
+
+                //////////////////////////
 
             //DateTime date = null;
 
