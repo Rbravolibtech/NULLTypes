ce3ffe638f8631f54fb9af6c0b200c285a979a81
Rbravolibtech <122310916+Rbravolibtech@users.noreply.github.com>
2024-01-09 12:11:30 -0700

    coded nullable date and time

--- Program.cs
+++ Program.cs
@@ -1,3 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
 
+namespace NULLTypes 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DateTime? date = new DateTime(2024, 1, 9);
+            DateTime? date2 = date.GetValueOrDefault();
+            DateTime? date3 = date2;
+
+            Console.WriteLine(date3.GetValueOrDefault());
+
+            //DateTime date = null;
+
+            //Console.WriteLine("GetValueorDefault(): " + date.GetValueOrDefault());
+            //Console.WriteLine("HasValue: " + date.HasValue);
+            //Console.WriteLine("Value: " + date.Value);
+        }
+    }
+}
